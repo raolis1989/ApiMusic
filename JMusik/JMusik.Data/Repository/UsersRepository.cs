8ed9974eae0f4ec02a59c1b545577f744fcb6135
Raolis <raolis1989@gmail.com>
2019-12-17 23:27:26 -0300

    implementacion-usuarios

---
+++ JMusik/JMusik.Data/Repository/UsersRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JMusik.Data.Repository
+{
+    public class UsersRepository
+    {
+    }
+}
---
+++ JMusik/JMusik.Dtos/UsuarioListDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JMusik.Dtos
+{
+    class UsuarioListDto
+    {
+    }
+}
